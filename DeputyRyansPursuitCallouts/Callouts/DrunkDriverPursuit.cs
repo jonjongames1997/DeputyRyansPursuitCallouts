a3685fa7bc5108cafc4a9c29982e71c05105abac
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-06 23:55:26 -0400

    Update DrunkDriverPursuit.cs

--- DeputyRyansPursuitCallouts/Callouts/DrunkDriverPursuit.cs
+++ DeputyRyansPursuitCallouts/Callouts/DrunkDriverPursuit.cs
@@ -1,23 +1,21 @@
 ﻿using CalloutInterfaceAPI;
-using LSPD_First_Response.Mod.Callouts;
-using Rage;
-using LSPD_First_Response.Mod.API;
 
 namespace DeputyRyansPursuitCallouts.Callouts
 {
     [CalloutInterface("Drunk Driver Pursuit", CalloutProbability.Medium, "A pursuit involving a drunk driver.", "Code 3", "LSPD")]
     public class DrunkDriverPursuit : Callout
     {
-        private Vector3 spawnPoint;
-        private Ped suspect;
-        private Vehicle suspectVehicle;
-        private Blip suspectBlip;
-        private LHandle pursuit;
+        private static Vector3 spawnPoint;
+        private static Ped suspect;
+        private static readonly Vehicle suspectVehicle;
+        private static Blip suspectBlip;
+        private static LHandle pursuit;
+        private static readonly string theVehicle[] = { "STANIER" };
 
         public override bool OnBeforeCalloutDisplayed()
         {
             spawnPoint = World.GetNextPositionOnStreet(Game.LocalPlayer.Character.Position.Around(500f));
-            suspectVehicle = new Vehicle("STANIER", spawnPoint);
+            suspectVehicle = new Vehicle(theVehicle[new Random().Next((int)theVehicle.Length)], spawnPoint);
 
             if (!suspectVehicle.Exists())
                 return false;
@@ -27,6 +25,7 @@ namespace DeputyRyansPursuitCallouts.Callouts
             ShowCalloutAreaBlipBeforeAccepting(spawnPoint, 30f);
             AddMinimumDistanceCheck(50f, spawnPoint);
 
+            CalloutInterfaceAPI.Functions.SendMessage(this, "Officer, a pursuit involving a drunk driver is in progress. The suspect is driving erratically. Proceed with caution.");
             CalloutMessage = "Drunk Driver Pursuit";
             CalloutPosition = spawnPoint;
             LSPD_First_Response.Mod.API.Functions.PlayScannerAudio("WE_HAVE CRIME_DRUNK_DRIVER");
@@ -38,16 +37,24 @@ namespace DeputyRyansPursuitCallouts.Callouts
         {
             suspectBlip = suspectVehicle.AttachBlip();
             suspectBlip.IsFriendly = false;
+            suspect.IsPersistent = true;
+            suspect.BlockPermanentEvents = true;
 
             suspect.Tasks.CruiseWithVehicle(suspectVehicle, 70f, VehicleDrivingFlags.FollowTraffic);
-
-            CalloutInterfaceAPI.Functions.SendMessage(this, "Officer, a pursuit involving a drunk driver is in progress. The suspect is driving erratically. Proceed with caution.");
 
             pursuit = LSPD_First_Response.Mod.API.Functions.CreatePursuit();
             LSPD_First_Response.Mod.API.Functions.AddPedToPursuit(pursuit, suspect);
             LSPD_First_Response.Mod.API.Functions.SetPursuitIsActiveForPlayer(pursuit, true);
 
             return base.OnCalloutAccepted();
+        }
+
+        // If the player does not accept the callout, it will clear this out //
+        public override void OnCalloutNotAccepted()
+        {
+            if (suspect) suspect.Delete();
+            if(suspectVehicle) suspectVehicle.Delete();
+            if(suspectBlip) suspectBlip.Delete();
         }
 
         public override void Process()
