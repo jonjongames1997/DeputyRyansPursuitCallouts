d42c1f46d13fb2df1c568471b6dec691213519bc
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-06 23:53:47 -0400

    Update Version.cs

--- DeputyRyansPursuitCallouts/VersionChecker/Version.cs
+++ DeputyRyansPursuitCallouts/VersionChecker/Version.cs
@@ -1,15 +1,11 @@
-﻿using Rage;
-using System;
-using System.Net;
-
-namespace DeputyRyansPursuitCallouts.VersionChecker
+﻿namespace DeputyRyansPursuitCallouts.VersionChecker
 {
     public class PluginCheck
     {
         // Check for updates
         public static bool IsUpdateAvailable()
         {
-            string curVersion = "Beta 4"; // Current version
+            string curVersion = Settings.PluginVersion;
             Uri latestVersionUri = new Uri("https://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=48930&textOnly=1"); // URL to store the current version
             WebClient webClient = new WebClient();
             string receivedData = string.Empty;
@@ -32,23 +28,23 @@ namespace DeputyRyansPursuitCallouts.VersionChecker
                 return false;
             }
 
-            if (receivedData != curVersion)
+            if (recieveData != Settings.PluginVersion)
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~DeputyRyansPursuitCallouts Warning", "~y~A new Update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~o~" + receivedData + "<br>~r~Please update to the latest build!");
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~DeputyRyansPursuitCallouts Warning", "~y~A new update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~y~" + recieveData + "<br>~r~Please Update to the latest build for new callouts and improvments!");
                 Game.Console.Print();
-                Game.Console.Print("================================================== DeputyRyansPursuitCallouts ===================================================");
+                Game.Console.Print("===================================================== DeputyRyansPursuitCallouts ===========================================");
                 Game.Console.Print();
-                Game.Console.Print("[WARNING]: A new version of DeputyRyansPursuitCallouts is available! Update to the latest build or play at your own risk.");
-                Game.Console.Print("[LOG]: Current Version:  " + curVersion);
-                Game.Console.Print("[LOG]: New Version:  " + receivedData);
+                Game.Console.Print("[WARNING!]: A new version of DeputyRyansPursuitCallouts is NOW AVAILABLE to download!");
+                Game.Console.Print("[LOG]: Current Version:" + curVersion);
+                Game.Console.Print("[LOG]: New Version:" + recieveData);
                 Game.Console.Print();
-                Game.Console.Print("================================================== DeputyRyansPursuitCallouts ===================================================");
+                Game.Console.Print("===================================================== DeputyRyansPursuitCallouts ===========================================");
                 Game.Console.Print();
                 return true;
             }
             else
             {
-                Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "~w~DeputyRyansPursuitCallouts", "", "Detected the ~g~latest~w~ build of ~y~DeputyRyansPursuitCallouts~w~!");
+                Game.DisplayNotification("3dtextures", "mpgroundlogo_cops", "DeputyRyansPursuitCallouts", "~g~Callouts Detected the latest build!");
                 return false;
             }
         }
