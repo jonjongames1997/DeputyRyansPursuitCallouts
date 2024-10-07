5af1222902811a8d9ab2795c278db2dd456b40ca
DeputyRyan <rybug5210@gmail.com>
2024-10-06 21:51:54 -0400

    Beta 4 Callout Fix

--- DeputyRyansPursuitCallouts/Main.cs
+++ DeputyRyansPursuitCallouts/Main.cs
@@ -11,20 +11,28 @@ namespace DeputyRyansPursuitCallouts
         {
             Functions.OnOnDutyStateChanged += OnDutyStateChanged;
             Game.LogTrivial("DeputyRyansPursuitCallouts has been initialized.");
+
+            // In-game notification log
+            Game.DisplayNotification("3dtextures", "mpgroundlogo_cops", "DeputyRyansPursuitCallouts", "~g~Callouts Initialized", "All callouts have been successfully loaded.");
         }
 
         private void OnDutyStateChanged(bool onDuty)
         {
             if (onDuty)
             {
-                Functions.RegisterCallout(typeof(PursuitInProgress));
-                Functions.RegisterCallout(typeof(LargeVehiclePursuit));
-                Functions.RegisterCallout(typeof(DrunkDriverPursuit));
+                // Register callouts when the player is on duty
                 Functions.RegisterCallout(typeof(ArmedRobberyGetaway));
-                Functions.RegisterCallout(typeof(StolenVehiclePursuit));
-                Functions.RegisterCallout(typeof(RecklessDriverPursuit));
-                Functions.RegisterCallout(typeof(VehicleTheftPursuit));
+                Functions.RegisterCallout(typeof(DrunkDriverPursuit));
                 Functions.RegisterCallout(typeof(HitAndRunPursuit));
+                Functions.RegisterCallout(typeof(LargeVehiclePursuit));
+                Functions.RegisterCallout(typeof(LargeVehiclePursuit2));
+                Functions.RegisterCallout(typeof(PursuitInProgress));
+                Functions.RegisterCallout(typeof(RecklessDriverPursuit));
+                Functions.RegisterCallout(typeof(RoadRagePursuit));
+                Functions.RegisterCallout(typeof(StolenPoliceVehiclePursuit));
+                Functions.RegisterCallout(typeof(StolenVehiclePursuit));
+                Functions.RegisterCallout(typeof(VehicleTheftPursuit));
+                Functions.RegisterCallout(typeof(VehicleTheftPursuit2));
             }
         }
 
