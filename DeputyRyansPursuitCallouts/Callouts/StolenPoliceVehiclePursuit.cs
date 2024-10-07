a65ce4bb3ca014d73a69a0536175b3313cdf37d0
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-07 00:10:51 -0400

    Update StolenPoliceVehiclePursuit.cs

--- DeputyRyansPursuitCallouts/Callouts/StolenPoliceVehiclePursuit.cs
+++ DeputyRyansPursuitCallouts/Callouts/StolenPoliceVehiclePursuit.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CalloutInterfaceAPI;
 
 namespace DeputyRyansPursuitCallouts.Callouts
 {
-    internal class StolenPoliceVehiclePursuit
+    public class StolenPoliceVehiclePursuit : Callout
     {
+
     }
 }
